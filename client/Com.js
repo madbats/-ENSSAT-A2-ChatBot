f868cf068fe3f921ba637762a16485181922cf22
Camille <cnicolas@enssat.fr>
2022-04-12 14:49:41 +0200

    Début communication avec serveur

--- client/Com.js
+++ client/Com.js
@@ -3,14 +3,14 @@ document.addEventListener('DOMContentLoaded',init);
 
 //Variables locales au script
 let message_container;
-let input_box;
+let input;
 let form;
 
 //Fonctions
 
 function init(){
     message_container = document.getElementById("messages");
-    input_box = document.getElementById("input");
+    input = document.getElementById("input");
     form = document.getElementById("form");
 
     form.addEventListener('submit', (e)=>{
@@ -18,12 +18,40 @@ function init(){
         console.log(message_container);
         //On ajoute le message de l'utilisateur dans la "file" des messages
         e.preventDefault();
-        message_container.innerHTML += `<div class="self">${input_box.value}</div>`;
+        message_container.innerHTML += `<div class="self">${input.value}</div>`;
         location.href='#edge';
+        
         //On envoie au serveur le message de l'utilisateur
-    
+        //Create the request
+		let myHeaders = new Headers();
+		myHeaders.append('Content-Type', 'application/json');
+		let payload = {
+       			message:input.value,
+       	};
+		console.log(`La tâche est assignée à :${payload.assignement}`);
+		let myBody = JSON.stringify(payload);
+		let myInit = { 
+			method: 'POST',
+       		headers: myHeaders,
+       		mode: 'cors',
+       		cache: 'default',
+       		body:myBody
+       	};
+       	let myURL = ""; // URL à définir
+       	//launch the request
+		fetch(myURL,myInit)
+        .then((httpResponse)=>{
+            return httpResponse.text();
+        })
+        .then((responseBody)=>{//Que fait on avec la réponse du serveur.
+            
+        })
+		.catch((err)=>{
+			console.log(`ERROR : ${err}`);
+		})	
+
         //On efface le texte écrit dans l'input
-        input_box.value='';
+        input.value='';
     })
 }
 
